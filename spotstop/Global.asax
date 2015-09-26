5c75f21242cad58ffb41e41bb01edc3c9802d0af
cowtongue <revisedboomerguy@gmail.com>
2015-09-26 16:20:07 -0500

    committed template

---
+++ spotstop/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="spotstop.MvcApplication" Language="C#" %>
---
+++ spotstop/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(spotstop.Startup))]
+namespace spotstop
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
